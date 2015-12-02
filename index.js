d3b8ac75edfffe9e641236e20d83caf349581458
Jesse Baird <jebaird@gmail.com>
2015-12-02 08:47:55 -0800

    inital commit, working generation

---
+++ index.js
@@ -0,0 +1,8 @@
+var myth = require('myth');
+var fs = require('fs');
+
+var css = fs.readFileSync('var.css', 'utf8');
+css+= fs.readFileSync('src/source.css', 'utf8');
+var converted = myth(css);
+
+fs.writeFileSync('converted.css', converted);
