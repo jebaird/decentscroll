ba37d585c95038feaf0d160424ac7d4ba9aa9633
Jesse Baird <jebaird@gmail.com>
2016-04-26 05:38:26 -0500

    added quick docs, changed file names to make more sense

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # decentscroll
-styled scrollbars using only css
+
+Author: Jesse Baird <jebaird@gmail.com>
+
+Styled scrollbars using only css. Uses myth.io as a postprocessor to generate a stylesheet that "normalizes" supported scrollbar styles across chrome and IE. Sadly Firefox doesn't support styling the native scrollbar via css.
+
+
+## Getting Started
+
+Run the following 
+
+* `nmp install`
+* `node index.js`
+
+Now include the generated stylesheet in your project:
+
+	<link rel="stylesheet" type="text/css" href="converted.css">  
+ 
+Or check out `test.html`
+
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log( 'generating..');
+/*
+    decentscroll
+    normalize scrollbar css for chrome and ie. Sorry does firefox support css styled scrollbars
+    https://github.com/jebaird/decentscroll
+    @author Jesse Baird <jebaird@gmail.com>
+    @license MIT
+
+ */
+console.log( 'generating decentscroll-converted.css ..');
 var myth = require('myth');
 var fs = require('fs');
 
@@ -6,6 +14,6 @@ var css = fs.readFileSync('var.css', 'utf8');
 css+= fs.readFileSync('src/source.css', 'utf8');
 var converted = myth(css);
 
-fs.writeFileSync('converted.css', converted);
+fs.writeFileSync('decentscroll-converted.css', converted);
 
 console.log( "done" );
